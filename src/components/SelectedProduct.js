7cef2ff24dd4728cce19f72d02b08277f9d12fc1
Ulug'bek Sotvoldiyev <dilafruztoshmatova209@gmail.com>
2025-08-21 13:09:37 +0500

    add select

--- src/components/SelectedProduct.js
+++ src/components/SelectedProduct.js
@@ -11,8 +11,8 @@ export default function SelectedProduct({selectProduct,setSelectProduct}) {
       <p className="text-sm text-gray-700">
          Buyurtma bekor qilish uchun quyidagi telefon raqam orqali biz bilan bog&#39;lanishingiz mumkin
       </p>
-      <p className="text-lg font-semibold mt-2">+998 (90) 305-77-83</p>
-      <div className="mt-4 flex justify-end">
+      <p className="text-lg font-semibold mt-2">+998 (99) 305-77-83</p>
+            <div className="mt-4 flex justify-end">
         <button onClick={() => setSelectProduct(false)} className="bg-green-600 text-white px-4 py-2 rounded">Yopish</button>
       </div>
     </DialogContent>
